10d82f517888f852e5561b2ffbfa3384d7e9a2a7
Fernando Augusto <f3rnand0_lp@hotmail.com>
2020-05-24 13:38:01 -0300

    Códigos

--- UDPClient.java
+++ UDPClient.java
@@ -4,9 +4,24 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Random;
 
 public class UDPClient {
+
+    private static Random rand = new Random();
+    private static char[] letras = "0123456789abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    public static String stringAleatoria(int tamanho) {
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < tamanho; i++) {
+            int ch = rand.nextInt (letras.length);
+            sb.append (letras [ch]);
+        }    
+        return sb.toString();    
+    }
+
     public static void main(String[] args) throws NoSuchAlgorithmException {
         // args give message contents and server hostname
         DatagramSocket aSocket = null;
@@ -17,19 +32,22 @@ public class UDPClient {
             int tamanhoMensagem = Integer.parseInt(args[1]); // Tamanho da mensagem em bytes que será gerada
             int repeticoes = Integer.parseInt(args[2]); // Número de vezes que a mensagem sera enviada
 
+            byte[] m = new byte[tamanhoMensagem]; // Cria o array de bytes
+            m = stringAleatoria(tamanhoMensagem).getBytes(); // Gera mensagem aleatória
+
             int serverPort = 6789;
+            Instant start = Instant.now();
             for(int i = 0; i < repeticoes; i++){
-
-                byte[] m = new byte[tamanhoMensagem]; // Cria o array de bytes
-                SecureRandom.getInstanceStrong().nextBytes(m); // Gera mensagem aleatória
-
                 DatagramPacket request = new DatagramPacket(m, m.length, aHost, serverPort);
                 aSocket.send(request);
                 byte[] buffer = new byte[100];
                 DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
                 aSocket.receive(reply);
-                System.out.println("Reply: " + new String(reply.getData()) + "\n");
+                System.out.println(i + " Reply: " + new String(reply.getData()) + "\n");
             }
+            Instant finish = Instant.now();
+            long timeElapsed = Duration.between(start, finish).toMillis();
+            System.out.println("Time Elapsed: " + timeElapsed);
         } catch (SocketException e) {
             System.out.println("SocketException: " + e.getMessage());
         } catch (IOException e) {
--- UDPServer.java
+++ UDPServer.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.SocketException;
 
 public class UDPServer{
